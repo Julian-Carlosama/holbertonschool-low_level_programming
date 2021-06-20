b13349c7f22fef2572841bd83c6f15d015504c4d
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-06-20 05:57:17 +0000

    owner

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- *Here inicialized witch main function
+ *main function
  *
- *printf : print on screen
+ *return : print on screen
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }
