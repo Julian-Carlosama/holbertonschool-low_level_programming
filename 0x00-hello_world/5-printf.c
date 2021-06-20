4ee763d12fbdb5a7efd69f5897dc06e811efd26a
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-06-20 06:26:58 +0000

    V1.8

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- *main function inicialized 
+ *main : Entry point 
  *
- *return : force the out 
+ *return : Always 0  
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, ");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
