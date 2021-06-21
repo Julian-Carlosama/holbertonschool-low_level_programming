2f6e077ec46d297429f08db23c9d5292ca268081
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-06-21 18:53:21 +0000

    V1

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Print all numbers of base 16
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 102; i++)
+	{
+		if ( i > 57 && i < 97)
+		{
+		}
+		else
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
