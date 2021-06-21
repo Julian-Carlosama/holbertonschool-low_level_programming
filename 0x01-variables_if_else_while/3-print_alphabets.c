aa3a2c5e651c68e931454f87461eba2056f988ff
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-06-21 03:06:18 +0000

    V1

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - show all alphabet
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alpa[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i = 0;
+
+	while (i < 52)
+	{
+		putchar(alpa[i]);
+		i++;
+	}
+	putchar('\n');
+}
