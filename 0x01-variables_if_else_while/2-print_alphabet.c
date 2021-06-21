c7603770726ebda8d8e08fec9a558d0b03a92ca9
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-06-21 02:42:52 +0000

    V2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - execute the alphabet
+ *
+ * Return: Always 0
+ */
+int main()
+{
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+	while (i < 26)
+	{
+		putchar(alpha[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
