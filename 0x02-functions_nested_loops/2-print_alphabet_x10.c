ca3acc85d7b3dacf1a9ddd8e4a53c496a6702906
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-06-22 16:28:23 +0000

    alphabet in 10 lines

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - Print alphabet 10 times
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 97; j <= 127; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
