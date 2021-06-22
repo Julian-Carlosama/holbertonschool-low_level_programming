91b77767fa821512c2868e29fb82687d3c9a15dd
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-06-22 06:01:55 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * main - print alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
