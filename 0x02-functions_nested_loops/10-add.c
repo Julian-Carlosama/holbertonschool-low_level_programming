5651ca3d660c31ebb686a6b28c047ff446a4f896
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-06-23 04:36:16 +0000

    Add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * add - Print the resul a+b
+ *
+ * @a: First number
+ *
+ * @b: Second number
+ *
+ * Return: show the result
+ */
+int add(int a, int b)
+{
+	int t = a + b;
+
+	return (t);
+	_putchar('\n');
+}
