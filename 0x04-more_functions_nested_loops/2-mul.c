598b6a9b402d6382ced8eb749153c8fb605d2602
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-06-26 04:03:59 +0000

    Multipique two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * mul - Compute variabe a and b
+ *
+ * @a: This is value to process
+ *
+ * @b: This  is other value to process
+ *
+ * Return: Show the result
+ */
+int mul(int a, int b)
+{
+	int r;
+
+	r = a * b;
+
+	return (r);
+}
