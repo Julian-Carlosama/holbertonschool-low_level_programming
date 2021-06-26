84be290ace2da0d04f1e2ecb8d711f9a24c9bfe1
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-06-26 01:38:00 +0000

    Uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * main - Print if c is upper
+ *
+ * @c: Is te value to process
+ *
+ * Return: 1 if c is upper else 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
