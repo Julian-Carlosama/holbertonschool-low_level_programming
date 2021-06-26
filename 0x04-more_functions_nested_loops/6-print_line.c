f9f91b77c66b8d0109a55dbdb713ef12760032be
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-06-26 04:52:11 +0000

    v3

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_line - print character
+ *
+ * @n: Is the number times
+ *
+ * Return: no return
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	while ( i < n)
+	{
+		_putchar(95);
+		i++;
+
+	}
+	_putchar('\n');
+}
