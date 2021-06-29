aa2fc3dfa9210e4150c4763ab70b03d0811d27e2
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-06-29 04:53:51 +0000

    Function that return a string long

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strlen - Returns the leng of the string
+ *
+ * @s: Is the string to process
+ *
+ * Return: return the string long
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i) != 0; i++)
+	{
+	}
+
+	return (i);
+}
