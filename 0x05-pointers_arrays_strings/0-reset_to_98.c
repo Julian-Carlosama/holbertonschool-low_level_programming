35c47f75da3f7ce65bc2b52bf7285abed3a4d64b
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-06-29 00:17:11 +0000

    This is a function that change the value of a variable for midle of a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - Change te value from variable n
+ *
+ * @n: Is a pointer of type integer
+ *
+ * Return: no return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
