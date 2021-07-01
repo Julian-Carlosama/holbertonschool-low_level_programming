cbf6016203728b3db6ed61d102cb94da07bba465
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-07-01 00:04:00 +0000

    Last numbers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * puts_half - Shows the last numbers
+ *
+ * @str: Is the string to process
+ *
+ * Return: no return
+ */
+void puts_half(char *str)
+{
+	int i = 0, j;
+
+	while (str[i] != '\0')
+		i++;
+
+	if (i % 2 == 0)
+		j = i / 2;
+	else
+		j = (i - 1) / 2;
+	for (j++; j < i; j++)
+		_putchar(str[j]);
+	_putchar(10);
+
+}
