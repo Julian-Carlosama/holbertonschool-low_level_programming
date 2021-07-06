0052ec91ca8bc76530e08d59b1be64d2a4b164e8
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-07-06 04:56:06 +0000

    localized a character that ocurrence in the string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strchr - Locates a character in a string
+ *
+ * @s: Is the pointer to process
+ *
+ * @c: Is the character to localize
+ *
+ * Return: If have coincidence or not
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	return (s);
+}
