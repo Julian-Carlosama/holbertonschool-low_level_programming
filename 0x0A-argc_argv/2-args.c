05a068539966a6dc42890d012d9453c5408d1dc1
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-07-10 04:56:25 +0000

    v3

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - print all arguments it receives
+ *
+ * @argc: is the elements number
+ *
+ * @argv: Is the array
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s \n", argv[i]);
+	return (0);
+}
