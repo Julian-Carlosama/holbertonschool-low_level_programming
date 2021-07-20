2150386a39051e3f21e138b754f63ebc986098c8
Julian Carlosama <cromocelcarlosama@gmail.com>
2021-07-19 23:53:43 -0500

    v1

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog.
+ *
+ * @name: dog name
+ *
+ * @age: dog age
+ *
+ * @owner: dog owner
+ *
+ * Return: no return
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	
+}
