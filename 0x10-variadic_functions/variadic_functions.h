654d7b3a231deacab0e3689a8546d6430e1e69b3
Julian Carlosama <cromocelcarlosama@gmail.com>
2021-07-24 23:43:56 -0500

    Header that contain prototypes for this project and struct of 3-print

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,17 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * struct print - struct
+ * @spec: the specification.
+ * @func: the function.
+ */
+typedef struct print_any
+{
+	char *spec;
+	void (*function)(va_list);
+} print_data;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
