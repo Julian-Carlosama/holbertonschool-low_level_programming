ff62b2b6c417dcf5fd7843ef00f7351be45ac497
Julian Carlosama <cromocelcarlosama@gmail.com>
2021-08-03 23:48:52 -0500

    More functions

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Deletes the head node of a linked list.
+ *
+ * @head: Head of the liked list.
+ *
+ * Return: 0 if the liked list is empty.
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *aux;
+
+	if (*head != NULL)
+	{
+		aux = *head;
+	}
+	return (aux);
+}
