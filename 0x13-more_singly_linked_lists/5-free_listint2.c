9148d2dc4971491a66862157947dd6ce788a23df
Julian Carlosama <cromocelcarlosama@gmail.com>
2021-08-03 22:28:31 -0500

    Function that frees the linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Free linked list
+ *
+ * @head: Head of the list
+ *
+ * Return: No return.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *aux;
+
+	if (head != NULL)
+	{
+		while (*head != NULL)
+		{
+			aux = (*head);
+			(*head) = (*head)->next;
+			free(aux);
+		}
+		free(*head);
+	}
+	*head = NULL;
+}
