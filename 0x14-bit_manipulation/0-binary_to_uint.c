bb9f94c02a9fb51644ab0dc37bd5b1f41b8e312f
Julian Carlosama <cromocelcarlosama@gmail.com>
2021-08-06 18:46:38 -0500

    Function that conver a string to binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Convert string in binary to
+ * integer.
+ *
+ * @b: Pointers to String to process
+ *
+ * Return: Integer number or NULL if
+ * is another thing.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int convert = 0;
+	int i;
+
+	if (b == NULL)
+		return (1);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != 48 && b[i] != 49)
+			return (2);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		convert <<= 1;
+		if (b[i] == 49)
+			convert += 1;
+	}
+	return (convert);
+}
