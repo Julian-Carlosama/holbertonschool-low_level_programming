53a9b2acca04b95f5c8bfca4de6812b52504d5bf
Julian Carlosama <cromocelcarlosama@gmail.com>
2021-08-06 21:47:47 -0500

    G

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation
+ * of a number.
+ *
+ * @n: number
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int x;
+	int z;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	for (z = ((sizeof(unsigned long int) * 8) - 1); z >= 0; z--)
+	{
+		x = n >> z;
+		if (x)
+		{
+			if (x & 1)
+				_putchar('1');
+			else
+				_putchar('0');
+		}
+	}
+}
