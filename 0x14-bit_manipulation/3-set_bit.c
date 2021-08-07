a36ca3b54e0a8a74cea82e1ca1e931f003bb5cf6
Julian Carlosama <cromocelcarlosama@gmail.com>
2021-08-06 23:38:22 -0500

    function that sets the value of a bit to 1 at a given index.

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,22 +4,23 @@
  * set_bit - that returns the number of bits you would
  * need to flip to get from one number to another.
  *
- * @n: 
+ * @n: Pointer to unsigned
  *
  * @index: Index is the index, starting from 0
  * of the bit you want to set
  *
+ * Return: 1 if it worked, or -1 if an error occurred.
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int number;
 
-	for (number = 0; n || m; nunmber >>= 1, m >>= 1)
-	{
-		if ((number & 1) != (m & 1))
-			nb++;
-	}
+	if (index > 63)
+		return (-1);
 
-	return (number);
+	number = 1 << index;
+	*n = (*n | number);
+
+	return (1);
 }
