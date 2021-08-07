45b4dd8c726fc68e750cbd7a3e8896ddbfb371dc
Julian Carlosama <cromocelcarlosama@gmail.com>
2021-08-06 23:18:24 -0500

    h

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * set_bit - that returns the number of bits you would
+ * need to flip to get from one number to another.
+ *
+ * @n: 
+ *
+ * @index: Index is the index, starting from 0
+ * of the bit you want to set
+ *
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int number;
+
+	for (number = 0; n || m; nunmber >>= 1, m >>= 1)
+	{
+		if ((number & 1) != (m & 1))
+			nb++;
+	}
+
+	return (number);
+}
