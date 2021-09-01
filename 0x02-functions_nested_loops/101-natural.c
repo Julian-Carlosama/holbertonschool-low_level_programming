7e012251dadc8d44f89ed34f493102b81828c3b5
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-09-01 15:09:56 -0500

    Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints naturals numbers below 1024 that are
+ * multiplies 3 or 5
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int x = 0, y = 0;
+
+	for (x = 1; i < 1024; x++)
+	{
+		if ((x % 3) == 0 || (a % 5) == 0)
+			y += x;
+	}
+	printf("%d\n", y);
+	return (0);
+}
